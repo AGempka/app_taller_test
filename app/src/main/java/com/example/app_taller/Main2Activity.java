37436a888211000a108793183d2045dd308bc973
Sebastian <sebas.bordon@hotmail.com>
2019-06-21 10:22:29 -0400

    a

--- app/src/main/java/com/example/app_taller/Main2Activity.java
+++ app/src/main/java/com/example/app_taller/Main2Activity.java
@@ -9,6 +9,7 @@ public class Main2Activity extends AppCompatActivity {
     public static final String nombre_cliente="nombre_cliente";
     TextView txtBienvenido;
     @Override
+    //jeje
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
